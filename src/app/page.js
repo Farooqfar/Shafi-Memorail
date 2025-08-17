16f1a21b8957399fa3dac25b9f472cd5cfc927a1
Farooqfar <info4895@gmail.com>
2025-08-17 20:04:07 +0500

    doctor

--- src/app/page.js
+++ src/app/page.js
@@ -8,20 +8,20 @@ import Services from "./components/Services";
 export default function Home() {
   return (
     <section className="w-[100%] h-[100%] overflow-hidden bg-[#FFFFFF]">
-      <main className="bg-gray-50 w-full min-h-screen selection:bg-green-400 selection:text-black max-lg:min-h-auto">
-        <header className="w-full h-20 pt-5 pb-5 border-b-1 border-gray-400 flex items-center  ">
+      <main className="bg-[url(/drarslan.png)] bg-cover max-md:bg-[url(/drArslanRes.png)] max-md:bg-center bg-norepeat w-full min-h-screen selection:bg-green-400 selection:text-black max-lg:min-h-auto">
 
+        <header className="w-full h-20 pt-5 pb-5 border-b-1 border-gray-400 flex items-center  ">
           <Navbar />
         </header>
-        <div className="w-full h-full flex justify-center items-center max-lg:flex-wrap max-lg:gap-3 ">
-          <div className="w-full mt-20 h-full overflow-hidden flex items-center justify-center max-lg:mt-0">
-            <div className="w-full p-10 flex flex-col items-center justify-center gap-5  max-lg:items-start">
-              <h1 className="text-black text-2xl uppercase ">we take care of your health</h1>
+        <div className="w-full h-full flex justify-start items-center max-lg:flex-wrap max-lg:gap-3">
+          <div className="w-full mt-20 h-full overflow-hidden flex items-center justify-start max-lg:mt-0">
+            <div className="w-full p-10 flex flex-col  gap-5  max-lg:items-start">
+              <h1 className="text-black text-2xl uppercase max-md:text-white ">we take care of your health</h1>
               <h1 className="text-[#1DD2C8] text-6xl font-bold max-lg:text-5xl">Dr.Muhammad Arslan Iqbal</h1>
-              <h1 className="text-black text-3xl max-lg:2xl">We Are Providing
+              <h1 className="text-black text-3xl max-lg:2xl max-md:text-white">We Are Providing
                 Best & Affordable
                 Health Care.</h1>
-              <p className="text-[#7F7B7A] text-lg text-center w-[80%] max-lg:w-full max-lg:text-start">We are committed to providing the best and most affordable healthcare, ensuring every patient receives compassionate, personalized treatment. Our mission is to deliver the highest quality medical services, combining advanced medical expertise with a patient-first approach. We believe that everyone deserves access to exceptional healthcare without compromising on quality, comfort, or dignity.</p>
+              <p className="text-sky-900 text-lg  w-[60%] max-lg:w-full max-lg:text-start">We are committed to providing the best and most affordable healthcare, ensuring every patient receives compassionate, personalized treatment. Our mission is to deliver the highest quality medical services, combining advanced medical expertise with a patient-first approach. We believe that everyone deserves access to exceptional healthcare without compromising on quality, comfort, or dignity.</p>
               <button className="w-50 bg-[#24CBC3] text-white px-6 py-3 rounded-xl font-medium shadow-md 
              hover:bg-[#1FA8A1] transition-colors cursor-pointer">Contact Us</button>
             </div>
